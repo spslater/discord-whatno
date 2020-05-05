8ee148ea3c5b5f3c53796e74739a051a58f6f3e9
Sean Slater <sean@spslater.com>
2020-05-04 17:32:36 -0700

    add some fixes

--- extension/comic-dl/comics.py
+++ extension/comic-dl/comics.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from os import makedirs, path, remove, system
+from os import makedirs, path, remove, system, path
 from bs4 import BeautifulSoup as bs
 from requests import get
-from urllib.request import urlretrieve as download
+from urllib.request import urlretrieve
 from urllib.parse import urlparse
 from yaml import load, Loader
 from PIL import Image, ImageFont, ImageDraw
-from textwrap import wrap
+from textwrap import fill
+from time import sleep
 
 def getNext(soup, nxtList):
 	soup = soup
@@ -23,13 +24,12 @@ def getNext(soup, nxtList):
 	return soup['href']
 
 def addAltToImage(inName, outName, altRaw):
-	comic = Image.open(inName)
+	comic = Image.open(inName).convert("RGBA")
 	c_width, c_height = comic.size
 
 	font = ImageFont.truetype('./font/Ubuntu-R.ttf', 16)
 	drawFont = ImageDraw.Draw(Image.new('RGB', (c_width, c_height*2), (255, 255, 255)))
-	altWrap = wrap(altRaw, width=(c_width-20)/8)
-	alt = '\n'.join(altWrap)
+	alt = fill(altRaw, width=(int((c_width-20)/8)))
 	alt_width, alt_height = drawFont.textsize(alt, font=font)
 
 	height = c_height+10+alt_height+10
@@ -42,7 +42,18 @@ def addAltToImage(inName, outName, altRaw):
 
 	output.save(outName)
 
-
+def download(img, saveAs, retries=5):
+	dur = 2
+	for x in range(1,retries+1):
+		try:
+			urlretrieve(img, saveAs)
+			break
+		except Exception as e:
+			print('\t' + type(ex).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
+			sleep(dur)
+			dur *= dur
+			if x == retries:
+				raise e
 
 with open('./comics.yml') as yml:
 	comics = load(yml.read(), Loader=Loader)
@@ -53,6 +64,9 @@ for comic in comics:
 	name = comic['name'] if ('name' in comic) else loc[:-1]
 	getAlt = comic['alt'] if ('alt' in comic) else False
 	nxtList = comic['nxt']
+
+	curCount = 0
+	maxCount = 25
 
 	while True:
 		soup = bs(get(url).text, 'html.parser')
@@ -72,6 +86,11 @@ for comic in comics:
 		except:
 			break
 
+		_, ext = path.splitext(img)
+		if ext.lower() != '.png':
+			url = nxt
+			continue
+
 		parts = url.split('/')
 		book = parts[-4].split('-')[1].zfill(2)
 		arc = parts[-3].split('-')[0].zfill(2)
@@ -84,19 +103,24 @@ for comic in comics:
 		saveRaw = dirs + "raw_" + book + arc + '_' + strip
 		saveAs = dirs + book + arc + '_' + strip
 
-		if not path.exists(saveRaw):
-			print(saveAs)
-			if getAlt and alt:
-				print('\tDownloading')
-				download(img, saveRaw)
-				print('\tAdding Alt Text')
-				addAltToImage(saveRaw, saveAs, alt)
-				print('\tRemoving raw')
-				remove(saveRaw)
-			else:
-				print('\tDownloading with no alt')
-				download(img, saveAs)
+		print(saveAs)
+		if getAlt and alt:
+			print('\tDownloading')
+			download(img, saveRaw)
+			print('\tAdding Alt Text')
+			addAltToImage(saveRaw, saveAs, alt)
+			print('\tRemoving raw')
+			remove(saveRaw)
+		else:
+			print('\tDownloading with no alt')
+			download(img, saveAs)
 
 		url = nxt
-	
+		if curCount == maxCount:
+			curCount = 0
+			print("Sleeping for 30 secs.")
+			sleep(30)
+		else:
+			curCount += 1
+
 	system('bash ./combine.sh ' + loc[:-1] + ' "' + name + '"')
