5c65c37604db57a9d414a9b71e6871eb430780b7
Sean Slater <sean@spslater.com>
2020-07-16 21:38:40 -0700

    Add ability to save tags and alt to db

--- extension/comic-dl/backfill.py
+++ extension/comic-dl/backfill.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-from os import makedirs, path, remove, system, chdir
+from os import makedirs, path, chdir
 from bs4 import BeautifulSoup as bs
 from requests import get
-from urllib.request import urlretrieve
 from urllib.parse import urlparse
 from yaml import load, Loader
-from PIL import Image, ImageFont, ImageDraw
-from textwrap import fill
 from time import sleep
 from tinydb import TinyDB, where
 
@@ -28,7 +25,7 @@ def getNext(soup, nxtList):
 
 def getTags(soup):
 	tagList = []
-	tags = soup.find('div', {'class':'post-comic'}).find('div', {'class':'post-tags'}).find_all('a')
+	tags = soup.find('div', {'class':'post-tags'}).find_all('a')
 	for tag in tags:
 		tagList.append(tag.text)
 	return tagList
@@ -39,38 +36,6 @@ def saveTags(imgName, tags, db):
 
 def addAltToDatabase(imgName, alt, db):
 	db.table('alts').insert({'img': imgName, 'alt': alt})
-
-def addAltToImage(inName, outName, altRaw):
-	comic = Image.open(inName).convert("RGBA")
-	c_width, c_height = comic.size
-
-	font = ImageFont.truetype('./font/Ubuntu-R.ttf', 16)
-	drawFont = ImageDraw.Draw(Image.new('RGB', (c_width, c_height*2), (255, 255, 255)))
-	alt = fill(altRaw, width=(int((c_width-20)/11)))
-	alt_width, alt_height = drawFont.textsize(alt, font=font)
-
-	height = c_height+10+alt_height+10
-	output = Image.new('RGBA', (c_width, height), (224, 238, 239, 255))
-
-	draw = ImageDraw.Draw(output)
-
-	output.paste(comic, (0,0), mask=comic)
-	draw.text((10,c_height+10), alt, font=font, fill="black")
-
-	output.save(outName)
-
-def download(img, saveAs, retries=5):
-	dur = 2
-	for x in range(1,retries+1):
-		try:
-			urlretrieve(img, saveAs)
-			break
-		except Exception as e:
-			print('\t' + type(e).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
-			sleep(dur)
-			dur *= dur
-			if x == retries:
-				raise e
 
 def getText(url, retries=10):
 	dur = 2
@@ -87,7 +52,7 @@ def getText(url, retries=10):
 def main():
 	chdir('/home/uniontown/projects/comics')
 
-	with open('./comics.yml') as yml:
+	with open('./backfill.yml') as yml:
 		comics = load(yml.read(), Loader=Loader)
 
 	for comic in comics:
@@ -141,36 +106,16 @@ def main():
 			arc = arcs[0].zfill(2)
 			arcName = '-'.join(arcs[1:])
 			strip = img.split('/')[-1]
-			dirs = loc + book + arc + '/'
 			imgName = loc[:-1] + '_' + book + arc + '_' + arcName + '_' + strip
 
+			print('Saveing Tags to Database')
 			tags = getTags(soup)
 			saveTags(imgName, tags, db)
 
-			if not path.isdir(dirs):
-				makedirs(dirs)
-
-			saveRaw = dirs + "raw_" + imgName
-			saveAs = dirs + imgName
-
-			print(saveAs)
 			if getAlt and alt:
-				print('\tDownloading')
-				download(img, saveRaw)
-				print('\tAdding Alt Text')
-				addAltToImage(saveRaw, saveAs, alt)
+				print('Saveing Alt to database')
 				addAltToDatabase(imgName, alt, db)
-				print('\tRemoving raw')
-				remove(saveRaw)
-			else:
-				print('\tDownloading with no alt')
-				download(img, saveAs)
 
-			zip_cmd = 'cd ' + dirs + ' && zip -ur ' + COMICS + '"' + name + '/' + name + ' - ' + book + arc + '.cbz" ' + imgName + ' > /dev/null'
-			zip_all = 'cd ' + dirs + ' && zip -ur ' + COMICS + '"' + name + '/' + name + '.cbz" ' + imgName + ' > /dev/null'
-			print('\tAdding to cbz')
-			system(zip_cmd)
-			system(zip_all)
 			print('\tDone')
 
 			if lastComic:
