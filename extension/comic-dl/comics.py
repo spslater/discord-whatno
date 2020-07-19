5e4e8ddd850ee807dcc8564e06c6e3b4a2b667de
Sean Slater <sean@spslater.com>
2020-07-19 16:54:41 -0700

    add logging, reorder / clean up stuff

--- extension/comic-dl/comics.py
+++ extension/comic-dl/comics.py
@@ -1,18 +1,61 @@
 #!/usr/bin/env python3
 
 from os import makedirs, path, remove, system, chdir
-from bs4 import BeautifulSoup as bs
-from requests import get
-from urllib.request import urlretrieve
-from urllib.parse import urlparse
-from yaml import load, Loader
-from PIL import Image, ImageFont, ImageDraw
+from sys import argv
 from textwrap import fill
 from time import sleep
-from sqlite3 import connect
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+
+import logging
 import re
 
-COMICS = "~/media/reading/webcomics/"
+from bs4 import BeautifulSoup as bs
+from PIL import Image, ImageFont, ImageDraw
+from requests import get
+from sqlite3 import connect
+from yaml import load, Loader
+
+def createTables(db : sqlite3.Cursor):
+	 db.executescript("""
+			CREATE TABLE Arc(
+				number PRIMARY KEY,
+				name TEXT UNIQUE NOT NULL,
+				url TEXT UNIQUE NOT NULL
+			);
+
+			CREATE TABLE Comic(
+				release PRIMARY KEY,
+				title TEXT NOT NULL,
+				image TEXT UNIQUE NOT NULL,
+				url TEXT UNIQUE NOT NULL,
+				arcId
+						REFERENCES Arc(rowid)
+						ON DELETE CASCADE
+						ON UPDATE CASCADE
+						NOT NULL
+			);
+
+			CREATE TABLE Alt(
+				comicId
+						UNIQUE
+						REFERENCES Comic(release)
+						ON DELETE CASCADE
+						ON UPDATE CASCADE
+						NOT NULL,
+				alt TEXT NOT NULL
+			);
+
+			CREATE TABLE Tag(
+				comicId
+						REFERENCES Comic(release)
+						ON DELETE CASCADE
+						ON UPDATE CASCADE
+						NOT NULL,
+				tag TEXT NOT NULL
+			);
+	 """)
+
 
 def getNext(soup, nxtList):
 	soup = soup
@@ -60,10 +103,11 @@ def download(img, saveAs, retries=5):
 			urlretrieve(img, saveAs)
 			break
 		except Exception as e:
-			print('\t' + type(e).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
+			logging.warning(type(e).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
 			sleep(dur)
 			dur *= dur
 			if x == retries:
+				logging.exception(e)
 				raise e
 
 def getText(url, retries=10):
@@ -72,181 +116,210 @@ def getText(url, retries=10):
 		try:
 			return get(url).text
 		except Exception as e:
-			print('\t' + type(ex).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
+			logging.warning(type(ex).__name__ + ' exception occured. Waiting for ' + dur + ' seconds to try again. Remaining atempts: ' + str(retries-x-1))
 			sleep(dur)
 			dur *= dur
 			if x == retries:
+				logging.exception(e)
 				raise e
 
-def addArc(db, img):
+def getTitle(soup):
+	return soup.find('h2', {'class': 'post-title'}).find('a').text
+
+def getArc(soup):
+		full = soup.find('li', {'class': 'storyline-root'}).find('a').text
+		return full.split(' - ')[1]	
+
+def addArc(db, img, fullname):
 	data = img.split('_')
 	num = data[1]
 	name = data[2]
 	url =   'https://www.dumbingofage.com/category/comic/book-' + (num[1] if num[0] == '0' else num) + '/' + num[2:3] + '-' + name + '/'
 
-	db.execute('SELECT * FROM Arc WHERE number=?', (num,))
+	db.execute('SELECT * FROM Arc WHERE number = ?', (num,))
 	row = db.fetchone()
 
 	if not row:
-		db.execute('INSERT INTO Arc VALUES (?,?,?)', (num, name, url))
-		db.execute('SELECT * FROM Arc WHERE number=?', (num,))
+		logging.debug('Inserting new arc: ' + fullname)
+		db.execute('INSERT INTO Arc VALUES (?,?,?)', (num, fullname, url))
+		db.execute('SELECT * FROM Arc WHERE number = ?', (num,))
 		row = db.fetchone()
 
 	return row
 
-def addComic(db, img, arc):
+def addComic(db, img, arc, fulltitle):
 	titleRelease = img.split('_')[3]
 	title = '.'.join('-'.join(titleRelease.split('-')[3:]).split('.')[:-1])
 	release = '-'.join(titleRelease.split('-')[0:3])
 	url = re.sub('category', release[0:4], arc[2], count=1) + title + '/'
 
-	db.execute('SELECT * FROM Comic WHERE release=?', (release,))
+	db.execute('SELECT * FROM Comic WHERE release = ?', (release,))
 	row = db.fetchone()
 
 	if not row:
+		logging.debug('Inserting new comic: ' + fulltitle)
 		try:
-			db.execute('INSERT INTO Comic VALUES (?,?,?,?,?)', (release, title, img, url, arc[0]))
+			db.execute('INSERT INTO Comic VALUES (?,?,?,?,?)', (release, fulltitle, img, url, arc[0]))
 		except:
 			url = url[:-2] + '-2' + url[-1]
-			db.execute('INSERT INTO Comic VALUES (?,?,?,?,?)', (release, title, img, url, arc[0]))
-		db.execute('SELECT * FROM Comic WHERE release=?', (release,))
+			db.execute('INSERT INTO Comic VALUES (?,?,?,?,?)', (release, fulltitle, img, url, arc[0]))
+		db.execute('SELECT * FROM Comic WHERE release = ?', (release,))
 		row = db.fetchone()
 
 	return row
 
 def addAlt(db, comic, alt):
-	db.execute('SELECT * FROM Alt WHERE comicId=?', (comic[0],))
+	db.execute('SELECT * FROM Alt WHERE comicId = ?', (comic[0],))
 	row = db.fetchone()
 
 	if not row:
+		logging.debug('Inserting new alt: ' + comic[0])
 		db.execute('INSERT INTO Alt VALUES (?,?)', (comic[0], alt))
-		db.execute('SELECT * FROM Alt WHERE comicId=?', (comic[0],))
+		db.execute('SELECT * FROM Alt WHERE comicId = ?', (comic[0],))
 		row = db.fetchone()
 
 	return row
 
 def addTag(db, comic, tag):
-	db.execute('SELECT * FROM Tag WHERE comicId=? AND tag=?', (comic[0], tag))
+	db.execute('SELECT * FROM Tag WHERE comicId = ? AND tag = ?', (comic[0], tag))
 	row = db.fetchone()
 
 	if not row:
 		db.execute('INSERT INTO Tag VALUES (?,?)', (comic[0], tag))
-		db.execute('SELECT * FROM Tag WHERE comicId=? AND tag=?', (comic[0], tag))
+		db.execute('SELECT * FROM Tag WHERE comicId = ? AND tag = ?', (comic[0], tag))
 		row = db.fetchone()
 
 	return row
 
+def getLevel(level):
+	if level == 'DEBUG':
+		return logging.DEBUG
+	elif level == 'INFO':
+		return logging.INFO
+	elif level == 'WARN':
+		return logging.WARNING
+	elif level == 'ERROR':
+		return logging.ERROR
+
 def main():
-	chdir('/home/uniontown/projects/comics')
+	workdir = argv[1]
+	savedir = argv[2]
+
+	chdir(workdir)
 
 	with open('./comics.yml') as yml:
-		comics = load(yml.read(), Loader=Loader)
+		comic = load(yml.read(), Loader=Loader)
 
-	for comic in comics:
-		cur = comic['cur'] if ('cur' in comic) else False
-		url = comic['home'] if (cur and 'home' in comic) else comic['url']
-		loc = comic['loc'] if ('loc' in comic) else (urlparse(url).netloc.split('.')[-2] + '/')
-		name = comic['name'] if ('name' in comic) else loc[:-1]
-		getAlt = comic['alt'] if ('alt' in comic) else False
-		nxtList = comic['nxt']
-		dbName = loc + comic['db']
+	cur = comic['cur'] if ('cur' in comic) else False
+	url = comic['home'] if (cur and 'home' in comic) else comic['url']
+	loc = comic['loc'] if ('loc' in comic) else (urlparse(url).netloc.split('.')[-2] + '/')
+	name = comic['name'] if ('name' in comic) else loc[:-1]
+	getAlt = comic['alt'] if ('alt' in comic) else False
+	nxtList = comic['nxt']
+	dbName = loc + (comic['db'] if ('db' in comic) else (name + '.db'))
+	output = loc + (comic['log'] if ('log' in comic) else 'output.log')
+	loglevel = getLevel(comic['level']) if ('level' in comic) else logging.INFO
 
-		conn = connect(dbName)
-		db = conn.cursor()
-		
-		lastComic = False
+	logging.basicConfig(format='%(asctime)s\t[%(levelname)s]\t%(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=log.INFO, filename=output)
 
-		curCount = 0
-		maxCount = 25
+	conn = connect(dbName)
+	db = conn.cursor()
+	
+	lastComic = False
 
-		while True:
-			print('\tGetting soup for ' + url)
-			soup = bs(getText(url), 'html.parser')
+	curCount = 0
+	maxCount = 25
 
-			try:
-				nxt = getNext(soup, nxtList)
-			except:
-				lastComic = True
+	while True:
+		logging.info('Getting soup for ' + url)
+		soup = bs(getText(url), 'html.parser')
 
-			imgTag = soup.find(id='comic').find('img')
+		try:
+			nxt = getNext(soup, nxtList)
+		except:
+			lastComic = True
 
-			img = imgTag['src']
-			alt = None
-			if getAlt:
-				if imgTag.get('alt', False):
-					alt = imgTag['alt']
-				elif imgTag.get('title', False):
-					alt = imgTag['title']
+		imgTag = soup.find(id='comic').find('img')
 
-			_, ext = path.splitext(img)
-			if ext.lower() != '.png':
-				url = nxt
-				continue
+		img = imgTag['src']
+		alt = None
+		if getAlt:
+			if imgTag.get('alt', False):
+				alt = imgTag['alt']
+			elif imgTag.get('title', False):
+				alt = imgTag['title']
 
-			if cur:
-				parts = getNext(soup, comic['book']).split('/')
-				book = parts[-3].split('-')[1].zfill(2)
-				arcs = parts[-2].split('-')
-			else:
-				parts = url.split('/')
-				book = parts[-4].split('-')[1].zfill(2)
-				arcs = parts[-3].split('-')
-			arc = arcs[0].zfill(2)
-			arcName = '-'.join(arcs[1:])
-			strip = img.split('/')[-1]
-			dirs = loc + book + arc + '/'
-			imgName = loc[:-1] + '_' + book + arc + '_' + arcName + '_' + strip
-
-			print('\tSaving Arc to Database')
-			arcRow = addArc(db, imgName)
-
-			print('\tSaving Comic to Database')
-			comicRow = addComic(db, imgName, arcRow)
-
-			print('\tSaving Tags to Database')
-			for tag in getTags(soup):
-				addTag(db, comicRow, tag)
-
-			if not path.isdir(dirs):
-				makedirs(dirs)
-
-			saveRaw = dirs + "raw_" + imgName
-			saveAs = dirs + imgName
-
-			if getAlt and alt:
-				print('\tDownloading')
-				download(img, saveRaw)
-				print('\tAdding Alt Text')
-				addAltToImage(saveRaw, saveAs, alt)
-				print('\tSaving Alt to Database')
-				addAlt(db, comicRow, alt)
-				print('\tRemoving raw')
-				remove(saveRaw)
-			else:
-				print('\tDownloading with no alt')
-				download(img, saveAs)
-
-			zip_cmd = 'cd ' + dirs + ' && zip -ur ' + COMICS + '"' + name + '/' + name + ' - ' + book + arc + '.cbz" ' + imgName + ' > /dev/null'
-			zip_all = 'cd ' + dirs + ' && zip -ur ' + COMICS + '"' + name + '/' + name + '.cbz" ' + imgName + ' > /dev/null'
-			print('\tAdding to cbz')
-			system(zip_cmd)
-			system(zip_all)
-			print('\tDone')
-
-			conn.commit()
-
-			if lastComic:
-				break
-
+		_, ext = path.splitext(img)
+		if ext.lower() != '.png':
 			url = nxt
-			if curCount == maxCount:
-				curCount = 0
-				print("Sleeping for 30 secs.")
-				sleep(30)
-			else:
-				curCount += 1
+			continue
 
-		conn.close()
+		if cur:
+			parts = getNext(soup, comic['book']).split('/')
+			book = parts[-3].split('-')[1].zfill(2)
+			arcs = parts[-2].split('-')
+		else:
+			parts = url.split('/')
+			book = parts[-4].split('-')[1].zfill(2)
+			arcs = parts[-3].split('-')
+		arc = arcs[0].zfill(2)
+		arcName = '-'.join(arcs[1:])
+		strip = img.split('/')[-1]
+		dirs = loc + book + arc + '/'
+		imgName = loc[:-1] + '_' + book + arc + '_' + arcName + '_' + strip
+
+		logging.info('Saving Arc to Database')
+		arcName = getArc(soup)
+		arcRow = addArc(db, imgName)
+
+		logging.info('Saving Comic to Database')
+		comicTitle = getTitle(soup)
+		comicRow = addComic(db, imgName, arcRow, comicTitle)
+
+		logging.info('Saving Tags to Database')
+		for tag in getTags(soup):
+			addTag(db, comicRow, tag)
+
+		if not path.isdir(dirs):
+			makedirs(dirs)
+
+		saveRaw = dirs + "raw_" + imgName
+		saveAs = dirs + imgName
+
+		if getAlt and alt:
+			logging.info('Downloading')
+			download(img, saveRaw)
+			logging.info('Adding Alt Text')
+			addAltToImage(saveRaw, saveAs, alt)
+			logging.info('Saving Alt to Database')
+			addAlt(db, comicRow, alt)
+			logging.info('Removing raw')
+			remove(saveRaw)
+		else:
+			logging.info('Downloading with no alt')
+			download(img, saveAs)
+
+		zip_cmd = 'cd ' + dirs + ' && zip -ur ' + savedir + '"' + name + '/' + name + ' - ' + book + arc + '.cbz" ' + imgName + ' > /dev/null'
+		zip_all = 'cd ' + dirs + ' && zip -ur ' + savedir + '"' + name + '/' + name + '.cbz" ' + imgName + ' > /dev/null'
+		logging.info('Adding to cbz')
+		system(zip_cmd)
+		system(zip_all)
+		logging.info('Done')
+
+		conn.commit()
+
+		if lastComic:
+			break
+
+		url = nxt
+		if curCount == maxCount:
+			curCount = 0
+			logging.debug("Sleeping for 30 secs.")
+			sleep(30)
+		else:
+			curCount += 1
+
+	conn.close()
 
 if __name__ == "__main__":
 	main()
