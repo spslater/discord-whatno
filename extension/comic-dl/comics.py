0ebbcb5b4f14c9cbfb64051bd5024ca9be3c9c1e
Sean Slater <sean@spslater.com>
2020-07-20 09:42:33 -0700

    update arc url

--- extension/comic-dl/comics.py
+++ extension/comic-dl/comics.py
@@ -133,7 +133,7 @@ def addArc(db, img, fullname):
 	data = img.split('_')
 	num = data[1]
 	name = data[2]
-	url = 'https://www.dumbingofage.com/category/comic/book-' + (num[1] if num[0] == '0' else num) + '/' + num[2:4] + '-' + name + '/'
+	url = 'https://www.dumbingofage.com/category/comic/book-' + (num[1] if num[0] == '0' else num[0:2]) + '/' + num[2:4] + '-' + name + '/'
 
 	db.execute('SELECT * FROM Arc WHERE number = ?', (num,))
 	row = db.fetchone()
