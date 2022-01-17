a23fd48285bfdcba8e59a34d9b1db9f4e902fedd
Sean Slater <seanslater@whatno.io>
2022-01-17 01:53:57 -0500

    Add new table to database for totals

--- cog-stats/stats/database.sql
+++ cog-stats/stats/database.sql
@@ -4,6 +4,13 @@ CREATE TABLE IF NOT EXISTS History(
     channel INTEGER NOT NULL,
     voicestate TEXT NOT NULL,
     starttime TIMESTAMP NOT NULL,
-    duration INTEGER NOT NULL,
+    duration REAL NOT NULL,
     historic BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Total(
+    user INTEGER NOT NULL,
+    channel INTEGER NOT NULL,
+    voicestate TEXT NOT NULL,
+    duration REAL NOT NULL
 );
